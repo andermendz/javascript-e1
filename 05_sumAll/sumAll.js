641b4a48569d6883d907ebac3c07ebfb00499286
andermendz <andersonmendz19@gmail.com>
2022-10-12 19:26:20 -0500

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    numbers = []
+    let sum = 0
+    let minor
+    let major
+
+    if (a < b && a > 0 && b > 0 && typeof a == 'number' && typeof b == 'number') {
+        minor = a
+        major = b
+    } else if (a > b && a > 0 && b > 0 && typeof a == 'number' && typeof b == 'number'){
+        minor = b
+        major = a
+    } else {
+        sum = "ERROR"
+    }
+
+    for (n = minor; n <=  major; n++) {
+        numbers.push(n)
+        sum += n
+    }
+    return sum
 };
 
 // Do not edit below this line
