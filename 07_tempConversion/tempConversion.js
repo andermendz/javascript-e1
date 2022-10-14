4b1ab243b3b5e342087fa38067fe1688b14a8d78
andermendz <andersonmendz19@gmail.com>
2022-10-13 20:40:11 -0500

    leapyear and tempconversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
+const ftoc = function (n) {
+  let value = (n - 32) * (5 / 9);
 
+  return parseFloat(value.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (n) {
+  let value = n * (9 / 5) + 32;
+  return parseFloat(value.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
